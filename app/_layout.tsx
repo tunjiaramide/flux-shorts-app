b21cc1ba16a56e8258d2ef9a20c6bb733f27d9d4
Adetunji Aramide <adetunjiaramide@gmail.com>
2025-09-03 05:04:39 +0100

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,9 @@
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
   return (
     <>
       <Stack 
